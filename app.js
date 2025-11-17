f63457b13b702bd610bf172d7d5e24951a73d2b0
Jakub Abram <j.abram1@wp.pl>
2025-11-17 11:14:19 +0100

    Created basic express app

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors({ credentials: true }))
+app.use(cookieParser())
+app.use(morgan("dev"))
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to the database.")
+        app.listen(3001, () => console.log("Listening for requests on port 3001"))
+    })
