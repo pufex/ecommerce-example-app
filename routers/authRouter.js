aa6d4bd358a057e869ab3eb80e15a0d6f4913c5f
Jakub Abram <j.abram1@wp.pl>
2025-11-17 12:10:50 +0100

    Changed name of authRouter

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { register, login, refresh, logout, confirmEmail, sendEmailToken} from "../controllers/authControllers"
 
-const expressRouter = express.Router()
+const authRouter = express.Router()
 
-expressRouter.post("/register", register)
-expressRouter.post("/login", login)
-expressRouter.get("/refresh", refresh)
-expressRouter.get("/logout", logout)
-expressRouter.post("/send-confirmation-email", sendEmailToken)
-expressRouter.post("/confirm-email", confirmEmail)
+authRouter.post("/register", register)
+authRouter.post("/login", login)
+authRouter.get("/refresh", refresh)
+authRouter.get("/logout", logout)
+authRouter.post("/send-confirmation-email", sendEmailToken)
+authRouter.post("/confirm-email", confirmEmail)
 
-export default expressRouter;
+export default authRouter;
