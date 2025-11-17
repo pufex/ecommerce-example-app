8f1297a3a2859aae95dc75c1ba78c6241e733e8c
Jakub Abram <j.abram1@wp.pl>
2025-11-17 11:24:24 +0100

    Added express.json() and express.urlencoded() middlewares

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const app = express()
 
 app.use(cors({ credentials: true }))
 app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(morgan("dev"))
 
 app.use("/auth", authRouter)
