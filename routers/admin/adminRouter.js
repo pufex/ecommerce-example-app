6b211c369bb33f70c87ac7fe3a375997409e8621
Jakub Abram <j.abram1@wp.pl>
2025-11-18 12:26:29 +0100

    Hooked up admin product controllers to the admin router

--- routers/admin/adminRouter.js
+++ routers/admin/adminRouter.js
@@ -1,10 +1,12 @@
 import express from "express"
+import {getProduct, getProducts, deleteProduct, editProduct, postProduct} from "../../controllers/admin/productControllers"
 
 const adminRouter = express.Router()
 
-adminRouter.get("/products", () => undefined)
-adminRouter.post("/create-product", () => undefined)
-adminRouter.delete("/delete-product", () => undefined)
-adminRouter.patch("/edit-product", () => undefined)
+adminRouter.get("/products", getProducts)
+adminRouter.get("/products/:id", getProduct)
+adminRouter.post("/create-product", postProduct)
+adminRouter.delete("/delete-product/:id", deleteProduct)
+adminRouter.patch("/edit-product/:id", editProduct)
 
 export default adminRouter;
