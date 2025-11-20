5031801c535646792ae96346552ce8864b462db1
Jakub Abram <j.abram1@wp.pl>
2025-11-20 11:05:09 +0100

    Fixed some stuff

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { register, login, refresh, logout, confirmEmail, sendEmailToken} from "../controllers/authControllers.js"
+import verifyJWT from "../middleware/verifyJWT.js"
 
 const authRouter = express.Router()
 
@@ -7,7 +8,7 @@ authRouter.post("/register", register)
 authRouter.post("/login", login)
 authRouter.get("/refresh", refresh)
 authRouter.get("/logout", logout)
-authRouter.post("/send-confirmation-email", sendEmailToken)
-authRouter.post("/confirm-email", confirmEmail)
+authRouter.post("/send-confirmation-email", verifyJWT, sendEmailToken)
+authRouter.post("/confirm-email", verifyJWT, confirmEmail)
 
 export default authRouter;
