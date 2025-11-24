9e094101f95ea3f0a9d14c84b2bc08823b422b29
Jakub Abram <j.abram1@wp.pl>
2025-11-24 15:11:39 +0100

    Updated the zod schema to include new address details properties

--- config/zodSchemas.js
+++ config/zodSchemas.js
@@ -14,9 +14,12 @@ export const loginSchema = z.object({
 export const detailsSchema = z.object({
     emailAddress: z.union([z.email(), z.null()]),
     phoneNumber: z.union([z.string(), z.null()]),
+    name: z.union([z.string(), z.null()]),
+    surname: z.union([z.string(), z.null()]),
     country: z.union([z.string(), z.null()]),
     state: z.union([z.string(), z.null()]),
     city: z.union([z.string(), z.null()]),
+    zipCode: z.union([z.string(), z.null()]),
     street: z.union([z.string(), z.null()]),
     houseNumber: z.union([z.string(), z.null()]),
     roomNumber: z.union([z.string(), z.null()]),
